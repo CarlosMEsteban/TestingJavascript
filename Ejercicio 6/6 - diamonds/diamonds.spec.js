a386820378c08730eabb82c53ffcb1ece1badc03
estgarca <estgarca@jcyl.es>
2022-06-16 10:25:05 +0200

    Ejercicio 6 primera versión

---
+++ Ejercicio 6/6 - diamonds/diamonds.spec.js
@@ -0,0 +1,23 @@
+async function mine(s) {
+  const promises = [];
+
+  while (s.includes('<>')) {
+    s = s.replace('<>', '');
+    promises.push(
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(1);
+        }, 100);
+      })
+    );
+  }
+
+  return (await Promise.all(promises)).length;
+}
+
+test('we can mine diamonds', async () => {
+  await expect(mine('')).resolves.toBe(0);
+  await expect(mine('<>')).resolves.toEqual(1);
+  await expect(mine('<<>>')).resolves.toEqual(2);
+  await expect(mine('<><<>><<')).resolves.toEqual(3);
+});
